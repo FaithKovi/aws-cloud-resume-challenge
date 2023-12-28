8355d7bd3e1ed1502c16f75e0685beac77f08e5e
FaithKovi <faithkovivera@gmail.com>
2023-12-28 12:33:49 +0100

    add bash script to clear and delete versioned s3 bucket

---
+++ infrastructure/s3-static-website/clear.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+import boto3
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('acrc-static-website112')
+bucket.object_versions.all().delete()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+aws s3 rm s3://acrc-static-website112/ --recursive
+
+
